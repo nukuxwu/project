cf4ef36a08d8d54dbe464046a2d204229fb5ed96
Chervyachok <example@gmail.com>
2023-09-14 12:52:58 +0300

    first commit

---
+++ js/script1.js
@@ -0,0 +1,150 @@
+//Переменные 
+// a = 15;
+// console.log(a);
+
+// let number = 5;
+// const leftBorderWeith = 1;
+
+// number = 10;
+// console.log(number);    
+
+// const obj = {
+// 	a:50
+// };
+
+// obj.a = 10;
+
+// console.log (obj);
+// console.log(name);
+// var name = "Chervyak";
+
+// {
+// 	var result = 50;
+// }
+
+// console.log(result);
+
+// alert (5);
+// [].push ("a");
+
+// let userName = "John";
+// let userNumber = 25;
+
+// userNumber=24;
+
+// console.log (userNumber,userName);
+
+// let number = 4.6;
+
+// console.log(-4/0);
+// console.log("string"*9);
+
+// const persone = "Chervyak";
+
+// const bool = false;
+
+// // console.log(something); null
+
+// let und;
+// console.log(und); undefined;
+
+// const obj = {
+// 	name:"Chervyak",
+// 	age:25,
+// 	isMarried: false
+// };
+
+// // console.log(obj.name);
+// console.log(obj["name"]);
+
+// let arr = ["plun.png", "red.jpg", 6,"apple.bmp"];
+// console.log(arr[1]);
+
+// Объекты и массивы
+
+// const arr = ["a", "b", "c"];
+
+// arr[10] = "1233";
+
+// console.log(arr);
+
+// const arrObj = {
+//     a: "a",
+//     "1": "b",
+//     2: "c",
+//     abc: {
+//         df:[{}, {}],
+//         def: {
+
+//         }
+//     }
+// }
+
+// const b = "b";
+
+// // arrObj.b = "12434";
+// arrObj["b"] = "12434";
+
+// console.log(arrObj["b"]);
+// console.log(arrObj.b);
+
+// const obj = {
+//     Chervyak:1000,
+//     "Perchik":500
+// };
+
+// Общение с пользователем
+
+// alert("Hello");
+
+// const result = confirm("Are you hear?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt("Как ваше имя?", "");
+// answers[1] = prompt("Как ваша фамилия?", "");
+// answers[3] = prompt("Сколько вам лет?", "");
+
+// console.log(typeof(answers)); 
+// console.log(typeof(null)); 
+
+// Интерполяция
+
+// const category = "toys";
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "chervyak";
+
+// alert (`Привет,${user}`);   
+
+// Операторы
+
+// console.log("arr" + " - object");
+// console.log(4 + +"5");
+
+// let incr = 10,
+// 	decr = 10;
+
+// // ++incr;
+// // --decr;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+
+// console.log(2*4 === 8);
+
+// console.log(2 + 2 * 2 === 8);
+
+// console.log(2 + 2 * 2 != 8);
+
+// const isCheked = false,
+// 	isClose = false;
+      
+// console.log(isCheked || !isClose);
