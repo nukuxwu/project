8c9f48c685eaf933ff7e7f79b3658b2d9c1b03b0
Chervyachok <example@gmail.com>
2023-09-15 12:30:58 +0300

    new material

--- js/worbook.js
+++ js/worbook.js
@@ -242,3 +242,70 @@
 // } else {
 //     console.log("мы уходим");
 // }
+// Циклы
+
+// let num = 50;
+
+// while(num < 55){
+// 	console.log(num);
+// 	num++;
+// }
+
+// do{
+// 	console.log(num);
+// 	num++;
+// }
+// while(num < 55);
+
+// for(let i = 1; i < 8; i++){
+// 	console.log(num);
+// 	num++;
+// }
+
+// for(let i = 1; i < 10; i++){
+// 	if(i === 6){
+// 		break;
+// 	}
+// 	console.log(i);
+// }
+
+// for(let i = 1; i < 10; i++){
+// 	if(i === 6){
+// 		continue;
+// 	}
+// 	console.log(i);
+// }
+
+// Вложенные циклы
+
+// for (let i = 0; i < 3; i++){
+// 	console.log(i);
+// 	for (let j = 0; j < 3; j++){
+// 		console.log(j);
+// 	}    
+// }
+
+// let result = "";
+// const length = 7;
+
+// for(let i = 1; i < length; i++){
+
+// 	for( let j = 0; j < i; j++){
+// 		result +="*";
+// 	}
+
+// 	result += "\n"; 
+// }
+
+// console.log(result);
+
+// first: for (let i = 0; i < 3; i++){
+// 	console.log(`First level: ${i}`);
+// 	for (let j = 0; j < 3; j++){
+// 		console.log(`Second level: ${j}`);
+// 	    for (let k = 0; k < 5; k++) {
+// 			if (k === 2) break first;
+// 			console.log(`Third level: ${k}`);
+// 		}
+// 	}  
+// }
