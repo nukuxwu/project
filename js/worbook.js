f01ac0d1094138f51597c44f9d81d6ce6cbdcf59
Chervyachok <example@gmail.com>
2023-09-18 17:08:06 +0300

    new material and new exercicse
    material: function and their arguments
    exercise: tasks for functions

--- js/worbook.js
+++ js/worbook.js
@@ -361,3 +361,112 @@
 //     }
 
 //     console.log(arrayOfNumbers);
+
+// Функции
+
+// function showFirstMessage(text){
+// 	console.log(text);
+// }
+
+// showFirstMessage("Hello world");
+
+// let num = 20;
+
+// function showFirstMessage(text){
+// 	console.log(text);
+// 	num = 10;
+// 	console.log(num);
+// }
+
+// showFirstMessage("Hello world");
+// console.log(num);
+
+
+// let num = 20;
+
+// function showFirstMessage(text){
+// 	console.log(text);
+// 	console.log(num);
+// }
+
+// showFirstMessage("Hello world");
+// console.log(num);
+
+// function calc(a, b){
+// 	return(a + b);
+// }
+
+// console.log(calc(2, 7));
+// console.log(calc(4, 8));
+// console.log(calc(3, 9));
+
+// function ret(){
+// 	let num = 50;
+// 	return num;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+// const logger = function(){
+// 	console.log("Hello")
+// };
+
+// logger();
+
+// const calc = (a, b) => {
+// 	console.log("1");
+// 	return a + b;
+// };
+
+// console.log(calc(2, 7));
+
+// Аргументы функций
+
+// const usdCurr = 28;
+
+// function convert(amount, curr){
+// 	console.log(curr * amount);
+// }
+
+// convert (500, usdCurr);
+
+
+// const usdCurr = 28;
+// const eurCurr = 32;
+
+// function convert(amount, curr){
+// 	console.log(curr * amount);
+// }
+
+// convert (500, usdCurr);
+// convert (500, eurCurr);
+
+
+
+// const usdCurr = 28;
+// const discount = 0.9;
+
+// function convert(amount, curr){
+// 	return curr * amount;
+// }
+
+// function promotion(result){
+// 	console.log(result * discount);
+// }
+
+// const res = convert(500, usdCurr);
+// promotion(res);
+// promotion(convert(500, usdCurr));
+
+// function test(){
+// 	for(let i = 0; i < 5; i++){
+// 		console.log(i);
+// 		if (i === 3) return;
+// 	}
+// 	console.log("Done");
+// }
+
+// test();
+// function doNothing() {}
+// console.log(doNothing() === undefined);
