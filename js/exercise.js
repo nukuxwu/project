23614abf2ac1720e7b5d7d13b544ef3d84fc9ad5
Chervyachok <example@gmail.com>
2023-09-18 17:08:06 +0300

    new material and new exercicse
    material: function and their arguments
    exercise: tasks for functions

--- js/exercise.js
+++ js/exercise.js
@@ -6,3 +6,59 @@ for(let i = 1; i <= 100; i++){
 }
 
 console.log(totalSum);
+
+function sayHello(name) {
+	return `Привет, ${name}!`;
+}
+
+console.log(sayHello("Pisun"));
+
+function returnNeighboringNumbers(num){
+	return [num - 1, num, num + 1];
+}
+
+console.log(returnNeighboringNumbers(5));	
+
+
+
+function getMathResult(num, times){
+	if (typeof(times) !== "number" || times <= 0) {
+		return num;
+	} 
+	let str ="";
+
+	for(let i = 1; i <= times; i++){
+		// console.log(`choto ${i}`);
+		// console.log(`choto${str}`);
+		if(i === times){
+			str += `${num *i}`;
+		} else {
+			str = str + num * i + "---";
+		}		
+	}
+	return str;
+}
+console.log(getMathResult(10, 5));	
+
+
+
+function squareSum(num){
+	return num.map(x => x ** 2).reduce((a, b) => a + b, 0);
+}
+
+const numbers = [1, 2, 4];
+const result = squareSum(numbers);
+console.log(result);
+
+console.log(Math.pow(3, 3));
+
+function trueNum (num){
+	if(num % 2 === 0){
+		return true;
+	} else{
+		return false;
+	}
+
+}
+
+console.log(trueNum(7));
