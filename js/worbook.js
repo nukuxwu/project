fc381d6ed227f65d989710905c029cf2048f1d8a
Chervyachok <example@gmail.com>
2023-09-21 16:11:04 +0300

    new material
    callback - function

--- js/worbook.js
+++ js/worbook.js
@@ -505,3 +505,34 @@
 // console.log(parseInt(test));(переводит число в другую систему исчисления)
 // console.log(parseFloat(test));(переводит число либо строку в десятичное значение(переводит строку в число))
 
+// Callback - функции
+
+// function first(){
+// 	// Do something
+// 	setTimeout(function(){
+// 		console.log(1);
+// 	}, 600);
+// }
+
+// function second(){
+// 	console.log(2);
+// }
+
+// first();
+// second();
+
+// function learnJS(lang, callback){
+// 	console.log(`Я учу: ${lang}`);
+// 	callback();
+// }
+
+
+//// function done(){
+//// 	console.log("Я прошел этот урок");
+//// }
+
+//// learnJS ("JavaScript", done);
+
+// learnJS ("JavaScript", function() {
+// 	console.log("Я прошел этот урок");
+// });
