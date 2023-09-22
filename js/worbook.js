96a9a38083b941a5e08a2796f79f36501dc34b5e
Chervyachok <example@gmail.com>
2023-09-22 13:40:23 +0300

    new material
    objects and object destructuring

--- js/worbook.js
+++ js/worbook.js
@@ -536,3 +536,51 @@
 // learnJS ("JavaScript", function() {
 // 	console.log("Я прошел этот урок");
 // });
+
+// Объекты,деструктуризация объектов
+
+// const options = {
+// 	name: "test",
+// 	width: 1024,
+// 	height: 1024,
+// 	colors: {
+// 		border: "black",
+// 		bg: "red"
+// 	},
+// 	makeTest: function(){
+// 		console.log("test");
+// 	}
+// };
+
+// console.log(options.name);
+
+// delete options.name;
+
+// console.log(options.colors.bg);
+
+// options.makeTest();
+
+// const {border, bg} = options.colors;
+// console.log(border);
+
+// console.log(Object.keys(options).length);
+// Получение ключей из объекта в массив +.length - получение количества элементов этого массива
+
+// console.log(options);
+
+// let counter = 0;
+
+// for (let key in options){
+// 	if (typeof(options[key]) === "object"){
+// 		for (let i in options[key]){
+// 			console.log(`Свойство ${i} имеет значение ${options[key][i]}`); => (options["colors"]["border"])		
+// 			counter++;
+// 		} 
+// 	} else {
+// 		console.log(`Свойство ${key} имеет значение ${options[key]}`);
+// 		counter++;
+// 	}
+// }
+
+// console.log(counter);
+
