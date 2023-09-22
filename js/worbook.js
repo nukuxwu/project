ae1e69bf3b5ac7dead31ba52366943bd7929c681
Chervyachok <example@gmail.com>
2023-09-22 14:58:14 +0300

    new material
    array methods and array iteration methods

--- js/worbook.js
+++ js/worbook.js
@@ -584,3 +584,48 @@
 
 // console.log(counter);
 
+// Методы массивов и методы перебора массивов 
+
+// const arr = [12, 24, 5, 8, 38];	
+// arr.sort(compareNum);
+// console.log(arr);
+
+// function compareNum(a, b){
+// 	return a - b;
+// }
+
+// arr.forEach(function (item, i, arr){
+// 	console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
+
+
+// arr[99] = 0; 
+// console.log(arr.length);
+// console.log(arr);
+
+// const poped = arr.pop();(удаляет последний элемент массива и возращает его значение)
+
+// arr.push(9);(добавляет заданный элемент в конец массива)
+
+// console.log(arr);
+// console.log(poped);
+
+// for(let i = 0; i < arr.length; i++){
+// 	console.log(arr[i]);
+// }
+
+// for (let value of arr){
+// 	console.log(value);
+// }
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+// console.log(products);
+
+// console.log(products.join("; "));
+
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+// products.sort();
+// console.log(products.join("; "));ы
