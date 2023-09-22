2decc172ced489fef490eca0677e706a7d3f9570
Chervyachok <example@gmail.com>
2023-09-22 16:31:24 +0300

    new material
    copy object with some changes
    used the operator "spead"(...)

--- js/worbook.js
+++ js/worbook.js
@@ -628,4 +628,101 @@
 // const str = prompt("", "");
 // const products = str.split(", ");
 // products.sort();
-// console.log(products.join("; "));ы
+// console.log(products.join("; "));
+
+//Передача данных по ссылке и по значению
+
+// let a = 5,
+// 	b = a;
+
+// b = b + 5;
+
+// console.log(b);
+// console.log(a);
+
+// const obj = {
+// 	a: 5,
+// 	b: 1
+// };
+
+// const copy = obj; //передача ссылки на существуюший объект
+
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+// function copy(mainObj){
+// 	let objCopy = {};
+
+// 	let key;
+// 	for(key in mainObj){
+// 		objCopy[key] = mainObj[key];
+// 	}
+
+// 	return objCopy;
+// }
+
+// const numbers = {
+// 	a: 2,
+// 	b: 5,
+// 	c:{
+// 		x:7,
+// 		y:4
+// 	}
+// };
+
+// const newNumbers = copy(numbers);
+
+// newNumbers.a = 10;
+// newNumbers.c.x = 10;
+
+// console.log(newNumbers);
+// console.log(numbers);
+
+// const add = {
+// 	d: 17,
+// 	e: 20
+// };
+
+// console.log(Object.assign(numbers, add));
+// const clone = Object.assign({}, add);
+
+// clone.d = 20;
+
+// console.log(add);
+// console.log(clone); 
+
+// const oldArr = ["a", "b", "c"];
+// const newArr = oldArr.slice();
+
+// newArr[1] = "asdasd"; 
+// console.log(newArr);
+// console.log(oldArr);
+
+// const video = ["youtube", "vimeo", "rutube"],
+// 	blogs = ["wordpress", "livejournal", "blogger"],
+// 	internet = [...video, ...blogs, "instagram", "facebook"];
+
+// console.log(internet);
+
+// function log(a, b, c){
+// 	console.log(a);
+// 	console.log(b);
+// 	console.log(c);
+// }
+
+// const num = [2, 4, 9];
+
+// log(...num);
+
+// const array = ["a", "b"];
+
+// const newArray = [...array];
+
+// const q = {
+// 	one: 1,
+// 	two: 2
+// };
+
+// const newObj = {...q};
