dcdb84eb8c0ca11571ca856aeea71705d3819220
Chervyachok <example@gmail.com>
2023-09-25 17:06:50 +0300

    new material
    object oriented prototype

--- js/worbook.js
+++ js/worbook.js
@@ -726,3 +726,31 @@
 // };
 
 // const newObj = {...q};
+
+
+// let str = "some";	
+// let strObj = new String(str);
+
+// console.log(typeof(str));
+// console.log(typeof(strObj));
+
+// const soldier = {
+// 	health: 400,
+// 	armor: 100,
+// 	sayHello: function() {
+// 		console.log("Hello");
+// 	}
+// };
+
+// const jonh = Object.create(soldier);
+
+// // const jonh = {
+// // health: 100
+// // };
+
+// // jonh.__proto__ = soldier;
+
+// Object.setPrototypeOf(jonh, soldier);
+
+// // console.log(jonh.armor); 
+// jonh.sayHello();
