d29e5f2119a3f185ab364c1ed34ca72fa5647624
Chervyachok <example@gmail.com>
2023-09-27 13:01:27 +0300

    new material
    interview tasks

--- js/worbook.js
+++ js/worbook.js
@@ -807,3 +807,45 @@
 
 // // 3)
 // console.log(typeof(!!"asdasd"));
+
+// Задачи с собеседований на понимание основ 
+
+// let x = 5;
+// alert( x++ );
+// 5
+
+// console.log([ ] + false - null + true) ;
+// NaN
+
+// let y = 1; 
+// let x = y = 2; 
+// alert(x); 
+// 2
+
+// console.log([ ] + 1 + 2);
+// 12
+
+// alert( "1"[0] );
+// 1
+
+// console.log(2 && 1 && null && 0 && undefined);
+// null
+//"И" запинается на лжи 
+ 
+// alert( null || 2 && 3 || 4 ); 
+// "Или" запинается на правде
+
+// const a = [1, 2, 3]; 
+// const b = [1, 2, 3]; 
+
+// console.log(a === b);
+// false
+
+// alert( +"Infinity" ); 
+// Infinity (number)
+
+// console.log("Ёжик" > "яблоко");
+// false
+
+// console.log(0 || "" || 2 || undefined || true || falsе );
+// 2
