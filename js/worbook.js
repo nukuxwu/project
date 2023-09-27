c2e5d85cf3879459da44a472a8ae9ddefbe8abd1
Chervyachok <example@gmail.com>
2023-09-27 16:27:43 +0300

    new material
    retrieve elements from a page

--- js/worbook.js
+++ js/worbook.js
@@ -849,3 +849,29 @@
 
 // console.log(0 || "" || 2 || undefined || true || falsе );
 // 2
+
+// const box = document.getElementById("box");
+
+// console.log(box);
+
+// const btns = document.getElementsByTagName("button");
+
+// console.log(btns[1]);
+
+// const btns = document.getElementsByTagName("button")[1];
+
+// console.log(btns);
+
+// const circles = document.getElementsByClassName("circle");
+
+// console.log(circles);
+
+// const hearts = document.querySelectorAll(".heart");
+
+// hearts.forEach(item => {
+// 	console.log(item);
+// });
+
+// const oneHeart = document.querySelector(".heart");
+
+// console.log(oneHeart);
