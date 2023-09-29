375c1d1d1590445582951edeba08ff268409f7c5
Chervyachok <example@gmail.com>
2023-09-29 13:35:57 +0300

    new material
    actions with elements on the page

--- js/worbook.js
+++ js/worbook.js
@@ -875,3 +875,57 @@
 // const oneHeart = document.querySelector(".heart");
 
 // console.log(oneHeart);
+
+// Действия с элементами на странице
+
+// const box = document.getElementById("box"),	
+// 	btns = document.getElementsByTagName("button"),	
+// 	circles = document.getElementsByClassName("circle"),	
+// 	hearts = document.querySelectorAll(".heart"),	
+// 	oneHeart = document.querySelector(".heart"),
+// 	wrapper = document.querySelector(".wrapper");
+
+// box.style.backgroundColor = "blue";
+// box.style.width = "500px";
+
+// box.style.cssText = "background-color: purple; width: 350px";
+
+// btns[1].style.borderRadius = "100%";
+// circles[0].style.backgroundColor = "red";
+
+// for (let i = 0; i < hearts.length; i++) {
+// 	hearts[i].style.backgroundColor = "pink";
+// }
+
+// hearts.forEach(item => {
+// item.style.backgroundColor = "pink";
+// });
+
+// const div = document.createElement("div");
+// const text = document.createTextNode("Тут был я");
+
+// div.classList.add("black");
+
+// wrapper.append(div);
+// wrapper.appendChild(div);
+
+// wrapper.prepend(div);
+
+// hearts[0].before(div);
+// hearts[0].after(div);
+
+// wrapper.insertBefore(div, hearts[1]); старый метод 
+
+// circles[0].remove();
+
+// wrapper.removeChild(hearts[1]); старый метод
+
+// hearts[0].replaceWith(circles[0]);
+
+// wrapper.replaceChild(circles[0], hearts[0]);
+
+// div.innerHTML = "<h1>Hello world</h1>";
+
+// div.textContent = "Hello";
+
+// div.insertAdjacentHTML("afterend", "<h2>Hello</h2>");
